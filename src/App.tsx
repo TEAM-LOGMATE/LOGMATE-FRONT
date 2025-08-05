c9904575faf88044ddd1443d2f16822774aecc8e
SeungWon Yang <ey8968@naver.com>
2025-08-06 02:23:04 +0900

    [Feat] seungwon: 테스트용 파일 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import { useState } from 'react';
-import BtnSign from './components/btn-sign';
-import BtnSnsLogin from './components/btn-sns-login';
-import BtnPoint from './components/btn-point';
-import BtnSetting from './components/btn-setting';
-import BtnAdd from './components/btn-add';
-import BtnLink from './components/btn-link';
-import BtnMore from './components/btn-more';
-import BtnMoreText from './components/btn-more-text';
-import BtnSmallArrow from './components/btn-small-arrow';
-import BtnBigArrow from './components/btn-big-arrow';
+import BtnSign from './components/btn/btn-sign';
+import BtnSnsLogin from './components/btn/btn-sns-login';
+import BtnPoint from './components/btn/btn-point';
+import BtnSetting from './components/btn/btn-setting';
+import BtnAdd from './components/btn/btn-add';
+import BtnLink from './components/btn/btn-link';
+import BtnMore from './components/btn/btn-more';
+import BtnMoreText from './components/btn/btn-more-text';
+import BtnSmallArrow from './components/btn/btn-small-arrow';
+import BtnBigArrow from './components/btn/btn-big-arrow';
+import IconBlind from './components/icon/icon-blind';
+import IconSign from './components/icon/icon-sign';
+import Input54 from './components/input/54';
+import Input48 from './components/input/48';
+import FrmMoreUser from './components/frm/frm-more-user';
+import FrmMoreTeam from './components/frm/frm-more-team';
+import AgentStatusUnresponsive from './components/text/agent-status-unresponse';
+import AgentStatusCollecting from './components/text/agent-status-collecting';
+import AgentStatusBefore from './components/text/agent-status-before';
+import AgentStatusStop from './components/text/agent-status-stop';
+import ErrorToast from './components/text/error-toast';
+import FrmFolder from './components/frm/frm-foler';
+import NaviBar from './components/navi/bar';
 
 export default function App() {
   const [disabled, setDisabled] = useState(false);
+  const [text, setText54] = useState('');
+  const [done, setDone] = useState(false);
+  const [code, setText48] = useState('');
 
   const handleClick = () => {
     if (!disabled) {
@@ -20,7 +36,7 @@ export default function App() {
   };
 
   return (
-    <div className="flex flex-col items-center gap-4 p-10 min-h-screen bg-brand-background text-white font-suit">
+    <div className="flex flex-wrap items-start gap-4 p-10 w-full bg-brand-background text-white font-suit">
       {/* 기본 로그인 버튼 */}
       <BtnSign disabled={disabled} onClick={handleClick}>
         로그인
@@ -64,6 +80,49 @@ export default function App() {
       {/* 화살표 표시 버튼 */}
       <BtnSmallArrow direction="up" />
       <BtnSmallArrow direction="down" />
+
+      {/* 아이콘 */}
+      <IconSign />
+      <IconBlind/>
+
+      {/* 입력창 */}
+      <Input54
+        value={text}
+        onChange={(e) => setText54(e.target.value)}
+        placeholder="글자 입력하고 엔터누르면 색 바꾸기"
+        done={done}
+        onDone={() => setDone(true)}
+      />
+
+      {/* 입력창2 */}
+      <Input48
+        value={code}
+        onChange={(e) => setText48(e.target.value)}
+        placeholder="팀 이름을 입력하세요"
+      />
+
+      {/* user */}
+      <FrmMoreUser />
+      {/* team */}
+      <FrmMoreTeam />
+
+
+      {/* Agent status 4가지 */}
+      <AgentStatusUnresponsive />
+
+      <AgentStatusCollecting />
+
+      <AgentStatusBefore/>
+
+      <AgentStatusStop/>
+
+      {/* 오류 텍스트 */}
+      <ErrorToast/>
+
+      {/* frm-folder -----미완성 */}
+      <FrmFolder/>
+
+      <NaviBar/>
     </div>
   );
 }
