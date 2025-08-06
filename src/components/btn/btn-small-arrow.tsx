c4e013f2dc91a647e5e2c8a52c15f97ca0d63191
SeungWon Yang <ey8968@naver.com>
2025-08-06 14:19:34 +0900

    [Fix] seungwon: smallarrow hover 상태 의존성 수정

--- src/components/btn/btn-small-arrow.tsx
+++ src/components/btn/btn-small-arrow.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-
 interface BtnSmallArrowProps {
   direction?: 'up' | 'down' | 'right' | 'left';
+  selected?: boolean;
 }
 
-export default function BtnSmallArrow({ direction = 'up' }: BtnSmallArrowProps) {
+export default function BtnSmallArrow({ direction = 'up', selected = false }: BtnSmallArrowProps) {
   let rotationClass = '';
 
   switch (direction) {
     case 'up':
-      rotationClass = 'rotate-[360deg]'; // 원래 위쪽
+      rotationClass = 'rotate-[360deg]';
       break;
     case 'down':
       rotationClass = 'rotate-180';
@@ -30,7 +29,6 @@ export default function BtnSmallArrow({ direction = 'up' }: BtnSmallArrowProps) 
         flex items-center justify-center
         w-[32px] h-[32px] aspect-square flex-shrink-0
         bg-transparent rounded-[4px] transition
-        group
         ${rotationClass}
       `}
     >
@@ -44,7 +42,7 @@ export default function BtnSmallArrow({ direction = 'up' }: BtnSmallArrowProps) 
       >
         <path
           d="M0.511772 8.56934C0.216966 8.31651 0.166638 7.88438 0.383842 7.57227L0.430717 7.51172L6.05083 0.955083C6.5497 0.373069 7.4504 0.373069 7.94927 0.955083L13.5694 7.51172C13.8389 7.82616 13.8027 8.29975 13.4883 8.56934C13.1739 8.83886 12.7003 8.80263 12.4307 8.48829L7.00005 2.15235L1.56939 8.48829L1.51665 8.54395C1.24137 8.80603 0.806585 8.82204 0.511772 8.56934Z"
-          className="fill-[#535353] group-hover:fill-[#F2F2F2] transition-colors"
+          className="fill-current transition-colors"
         />
       </svg>
     </button>
