c5b488bcbf05d405e99182cbb30e8bf13c589750
SeungWon Yang <ey8968@naver.com>
2025-08-07 15:25:44 +0900

    [Refactor] seungwon: BtnSign에서 disabled->isActive로 prop변경 및 스타일 로직 수정

--- src/components/btn/btn-sign.tsx
+++ src/components/btn/btn-sign.tsx
@@ -1,21 +1,20 @@
 type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
-  disabled?: boolean;
+  isActive?: boolean;
 };
 
 export default function BtnSign({
   children,
   onClick,
-  disabled = false,
+  isActive = true,
 }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      disabled={disabled}
       className={`
         flex items-center justify-center
-        w-[520px] h-[54px]
+        w-[480px] h-[54px]
         px-[239px] py-[15px]
         gap-[10px]
         shrink-0
@@ -24,21 +23,22 @@ export default function BtnSign({
         font-suit text-base leading-6 tracking-[-0.4px]
         whitespace-nowrap transition-all duration-150
         ${
-          disabled
+          isActive
             ? `
-            border border-[#1F5A05]
-            bg-transparent
-            text-[#2BBD39]
-            font-medium
-            cursor-not-allowed
-          `
-            : `
             bg-[#4FE75E]
             text-[#184405]
             font-bold
             hover:bg-[#42FF55]
             hover:border hover:border-[#2BBD39]
             hover:text-[#184405]
+            cursor-pointer
+          `
+            : `
+            border border-[#1F5A05]
+            bg-transparent
+            text-[#2BBD39]
+            font-medium
+            cursor-default
           `
         }
       `}
