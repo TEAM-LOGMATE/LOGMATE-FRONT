b756dbbdc2c10281ee63e8e089f1b05aced48613
SeungWon Yang <ey8968@naver.com>
2025-08-08 03:27:59 +0900

    [Feat] seungwon: MainPage.tsx 추가로 인한 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion'; 
 import LoginPage from './pages/login/LoginPage';
 import ComponentTest from './pages/component-test';
 import SignupPage from './pages/signup/SignupPage';
+import MainPage from './pages/home/MainPage';
+
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/component-test" element={<ComponentTest />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/main" element={<MainPage />} /> 
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/component-test" element={<ComponentTest />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
