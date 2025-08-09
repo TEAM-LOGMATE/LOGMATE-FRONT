96925dcbfd71ae2e0da68c2fb083195e6d048c86
SeungWon Yang <ey8968@naver.com>
2025-08-10 02:35:45 +0900

    [Feat] seungwon: 랜딩페이지 제작을 위한 라우터 분리

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,60 @@ import { AuthProvider, ProtectedRoute } from './utils/AuthContext';
 function AnimatedRoutes() {
   const location = useLocation();
   return (
-      <Routes location={location} key={location.pathname}>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route
-          path="/personal"
-          element={
-            <ProtectedRoute fallback={<div style={{ color: '#fff' }}>Loading...</div>}>
-              <PSpacePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/myinfo"
-          element={
-            <ProtectedRoute fallback={<div style={{ color: '#fff' }}>Loading...</div>}>
-              <MyInfoPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/edit-info"
-          element={
-            <ProtectedRoute fallback={<div style={{ color: '#fff' }}>Loading...</div>}>
-              <MyInfoEditPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<LoginPage />} />
-      </Routes>
-    // </AnimatePresence>
+    <Routes location={location} key={location.pathname}>
+      {/* 첫 화면: 랜딩페이지 */}
+      <Route
+        path="/"
+        element={
+          <div
+            style={{
+              color: '#fff',
+              fontSize: '24px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh',
+              backgroundColor: '#000',
+            }}
+          >
+            랜딩페이지
+          </div>
+        }
+      />
+
+      {/* 로그인/회원가입 */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      {/* 보호된 페이지 */}
+      <Route
+        path="/personal"
+        element={
+          <ProtectedRoute fallback={<div style={{ color: '#fff' }}>Loading...</div>}>
+            <PSpacePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/myinfo"
+        element={
+          <ProtectedRoute fallback={<div style={{ color: '#fff' }}>Loading...</div>}>
+            <MyInfoPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/edit-info"
+        element={
+          <ProtectedRoute fallback={<div style={{ color: '#fff' }}>Loading...</div>}>
+            <MyInfoEditPage />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* 그 외 경로는 로그인 페이지로 */}
+      <Route path="*" element={<LoginPage />} />
+    </Routes>
   );
 }
 
