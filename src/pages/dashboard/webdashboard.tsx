18e6fa6b2a5e6df02d9d047de79eb827c4827e40
SeungWon Yang <ey8968@naver.com>
2025-08-19 03:27:03 +0900

    [Feat] seungwon: 웹로그 대시보드 화면 구현

---
+++ src/pages/dashboard/webdashboard.tsx
@@ -0,0 +1,41 @@
+// src/pages/dashboard/WebDashboard.tsx
+import { useState } from "react";
+import WebAll from "../component/WebAll";
+import WebAI from "../component/WebAI";
+import WebErrorLog from "../component/WebErrorlog";
+import WebPath from "../component/WebPath";
+import SearchRefresh from "../component/searchrefresh";
+import WebLiveLog from "../component/WebLiveLog";
+import WebLogLine from "../component/WebLogLine";
+import WebTimeLog from "../component/WebTimeLog";
+
+export default function WebDashboard() {
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  return (
+    <div className="flex flex-col gap-6">
+      {/* 상단 카드 묶음 (그대로 유지) */}
+      <div className="w-full max-w-[1385px] flex gap-2 flex-shrink-0">
+        {/* 총 로그수 + 에러 로그 묶음 */}
+        <div className="flex flex-col gap-3">
+          <WebAll />
+          <WebErrorLog />
+        </div>
+
+        {/* AI 이상 탐지 + 상위 10 Path */}
+        <div className="flex gap-3">
+          <WebAI />
+          <WebPath />
+        </div>
+      </div>
+
+      {/* 카드 섹션 */}
+      <div className="w-full max-w-[1385px] flex flex-col gap-6">
+        <SearchRefresh onRefresh={() => setRefreshKey((k) => k + 1)} /> 
+        <WebLiveLog key={refreshKey} />  
+        <WebLogLine />
+        <WebTimeLog />
+      </div>
+    </div>
+  );
+}
