75536ae819e942083e576f3ebeb06ba04b6ac246
SeungWon Yang <ey8968@naver.com>
2025-08-19 03:06:13 +0900

    [Feat] seungwon: 드롭다운 버튼 컴포넌트 추가

---
+++ src/components/btn/btn-dropdown.tsx
@@ -0,0 +1,23 @@
+export default function BtnDropdown() {
+  return (
+    <button
+      className="
+        w-[24px] h-[24px]
+        flex items-center justify-center
+        p-0 bg-transparent border-none
+      "
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        className="w-6 h-6"
+        fill="none"
+      >
+        <path
+          d="M11.7474 16.1399L6.95119 10.6585C6.38543 10.0119 6.84461 9 7.70377 9H17.2962C18.1554 9 18.6146 10.0119 18.0488 10.6585L13.2526 16.1399C12.8542 16.5952 12.1458 16.5952 11.7474 16.1399Z"
+          fill="#AEAEAE"
+        />
+      </svg>
+    </button>
+  );
+}
